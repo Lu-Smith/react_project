8676361464dec7226ae3d11f809be18413db9d8c
Lu-Smith <104765947+Lu-Smith@users.noreply.github.com>
2022-08-08 18:27:51 +0100

    Updated styling in Popular.jsx

--- src/components/Popular.jsx
+++ src/components/Popular.jsx
@@ -34,6 +34,7 @@ function Popular() {
                                    <Card>
                                       <p>{recipe.title} {recipe.vegan === false ? (null) : ("(Vegan ✅)")} </p>
                                       <img src={recipe.image} alt={recipe.title} />
+                                      <Gradient />
                                    </Card>
                             </SplideSlide>
                         );
@@ -52,10 +53,43 @@ const Card = styled.div`
     min-height: 25rem;
     border-radius: 2rem;
     overflow: hidden;
+    position: relative;
+
 
     img {
         border-radius: 2rem;
+        position: absolute;
+        left: 0;
+        width: 100%;
+        height: 100%;
+        object-fit: cover;
+    }
+
+    p {
+        position: absolute;
+        z-index: 10;
+        left: 50%;
+        bottom: 0%;
+        transform: translate(-50%, 0%);
+        color: white;
+        text-align: center;
+        font-weight: 600;
+        font-size: 1rem;
+        height: 40%;
+        display: flex;
+        justify-content: center;
+        align-items: center;
     }
 `
+
+const Gradient = styled.div`
+    z-index: 3;
+    position: absolute;
+    width: 100%;
+    height: 100%;
+    background: linear-gradient(rgba(0, 0, 0, 0), rgba(0, 0, 0, 0.5));
+
+`
+
 
 export default Popular;
