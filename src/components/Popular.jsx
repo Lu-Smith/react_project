8a0500d1ccd84008e77655a39a50efdfaee7ddb5
Lu-Smith <104765947+Lu-Smith@users.noreply.github.com>
2022-08-08 16:08:40 +0100

    Connected API

--- src/components/Popular.jsx
+++ src/components/Popular.jsx
@@ -1,6 +1,16 @@
-import React from 'react'
+import React, {useEffect} from 'react';
 
 function Popular() {
+
+    useEffect(() => {
+        getPopular();
+    }, []);
+
+    const getPopular = async () => {
+        const api = await fetch(`https://api.spoonacular.com/recipes/random?apiKey=e86dacad0dc243289d1377a4b5dd2776&number=9`);
+        const data = await api.json();
+        console.log(data);
+    }
   return (
     <div>Popular</div>
   )
