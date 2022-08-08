753b95e98b9d75cda5b2f828925f0a0120d271fc
Lu-Smith <104765947+Lu-Smith@users.noreply.github.com>
2022-08-08 17:39:29 +0100

    Updated Popular.jsx

--- src/components/Popular.jsx
+++ src/components/Popular.jsx
@@ -1,18 +1,28 @@
-import React, {useEffect} from 'react';
+import React, {useEffect, useState } from 'react';
+
 
 function Popular() {
-
+    const [popular, setPopular] = useState([]);
     useEffect(() => {
         getPopular();
-    }, []);
+    }, [])
 
     const getPopular = async () => {
-        const api = await fetch(`https://api.spoonacular.com/recipes/random?apiKey=e86dacad0dc243289d1377a4b5dd2776&number=9`);
+        const api = await fetch(`https://api.spoonacular.com/recipes/random?apiKey=e86dacad0dc243289d1377a4b5dd2776&number=9`)
         const data = await api.json();
-        console.log(data);
+        setPopular(data.recipes);
+        console.log(data.recipes);
     }
   return (
-    <div>Popular</div>
+    <div>
+        {popular.map((recipe) => {
+            return(
+             <div key={recipe.id}>
+                <p>{recipe.title}</p>
+             </div>
+            );
+        })}
+    </div>
   )
 }
 
