dc8c35bcfea2ec32d9a550285a8bf375c055e3d7
Lu-Smith <104765947+Lu-Smith@users.noreply.github.com>
2022-08-08 21:36:11 +0100

    Updated Search.jsx

--- src/components/Search.jsx
+++ src/components/Search.jsx
@@ -3,11 +3,19 @@ import { FaSearch } from 'react-icons/fa';
 import { FormStyle } from '../styles/Search.styled';
 
 function Search() {
+const [input, setInput] = useState("");
+
+const submitHandler = (e) => {
+    e.preventDefault();
+    console.log("hey");
+};
+
   return (
-    <FormStyle>
+    <FormStyle onSubmit={submitHandler}>
         <div>
         <FaSearch></FaSearch>
-        <input type="text" />
+        <input type="text" value={input} onChange={(e) => setInput(e.target.value)}/>
+        <h4>{input}</h4>
         </div>
     </FormStyle>
   )
