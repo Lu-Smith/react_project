c68c4e98b4f3c1ce9875e6f2d5a83a0979ba17af
Lu-Smith <104765947+Lu-Smith@users.noreply.github.com>
2022-08-09 10:55:48 +0100

    Updated Recipe.jsx
    Corrected the path name

--- src/pages/Recipe.jsx
+++ src/pages/Recipe.jsx
@@ -1,12 +1,23 @@
-import { useState, useEffect } from "react";
+import React, { useState, useEffect } from "react";
 import { useParams } from "react-router-dom";
 
-
-import React from 'react'
-
 function Recipe() {
+
+    let params = useParams();
+    const [details, setDetailes] = useState({});
+
+const fetchDetails = async () => {
+    const data = await fetch(`https://api.spoonacular.com/recipes/${params.name}/information?apiKey=e86dacad0dc243289d1377a4b5dd2776`);
+    const detailData = await data.json();
+    setDetailes(detailData);
+}
+
+useEffect(() => {
+   fetchDetails();
+}, [params.name]);
+
   return (
-    <div>Recipe</div>
+    <div>{details.title}</div>
   )
 }
 
