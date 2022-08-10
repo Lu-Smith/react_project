003c966b73651e49fef4e3809cf94c6a676680c6
Lu-Smith <104765947+Lu-Smith@users.noreply.github.com>
2022-08-10 15:22:53 +0100

    Updated layout

--- src/styles/Search.styled.js
+++ src/styles/Search.styled.js
@@ -2,7 +2,7 @@ import styled from "styled-components";
 
 export const FormStyle = styled.form`
  margin: 0 auto;
-margin: 0 20rem;
+ max-width: 600px;
 
 @media screen and (max-width: 820px) {
   min-width: 300px;
@@ -21,11 +21,15 @@ input {
     background: linear-gradient(35deg, #494949, #313131);
     font-size: 1.5rem;
     color: #fff;
-    padding: 1rem 3rem;
+    padding: 1rem 5rem;
     border: none;
     border-radius: 1rem;
     outline: none;
     width: 100%;
+
+    @media screen and (max-width: 820px) {
+      padding: 1rem 3.5rem;
+    }
 }
 
 svg {
