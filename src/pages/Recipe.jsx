fb77c3b08b7bd753d081a02bcd32a7bdb45dc31b
Lu-Smith <104765947+Lu-Smith@users.noreply.github.com>
2022-08-10 11:36:02 +0100

    Updated Recipe.jsx
    Fixed useEffect warning

--- src/pages/Recipe.jsx
+++ src/pages/Recipe.jsx
@@ -5,17 +5,18 @@ import { Button, DetailedWrapper, Info } from "../styles/Recipe.styled";
 function Recipe() {
 
     let params = useParams();
-    const [details, setDetailes] = useState({});
+    const [details, setDetails] = useState({});
     const [activeTab, setActiveTab] = useState('instractions');
 
-const fetchDetails = async () => {
-    const data = await fetch(`https://api.spoonacular.com/recipes/${params.name}/information?apiKey=e86dacad0dc243289d1377a4b5dd2776`);
-    const detailData = await data.json();
-    setDetailes(detailData);
-}
+
 
 useEffect(() => {
-   fetchDetails();
+  const fetchDetails = async () => {
+    const data = await fetch(`https://api.spoonacular.com/recipes/${params.name}/information?apiKey=e86dacad0dc243289d1377a4b5dd2776`);
+    const detailData = await data.json();
+    setDetails(detailData);
+  }
+  fetchDetails();
 }, [params.name]);
 
   return (
@@ -27,6 +28,19 @@ useEffect(() => {
       <Info>
         <Button className={activeTab === 'instractions' ? 'active' : ''} onClick={() => setActiveTab('instractions')}>Instructions</Button>
         <Button className={activeTab === 'ingredients' ? 'active' : ''} onClick={() => setActiveTab('ingredients')}>Ingredients</Button>
+      {activeTab === 'instractions' && (
+        <div>
+          <h3 dangerouslySetInnerHTML={{__html: details.summary}}></h3>
+          <h3 dangerouslySetInnerHTML={{__html: details.instractions}}></h3>
+        </div>
+      )}
+      {activeTab === 'ingredients' && (
+           <ul>
+           {details.extendedIngredients.map((ingredient) => (
+             <li key={ingredient.id}>{ingredient.original}</li>
+           ))}
+         </ul>
+      )}
       </Info>
     </DetailedWrapper>
   )
