6381a6ebca0fa217db22a80f50b817d09c6f8750
Lu-Smith <104765947+Lu-Smith@users.noreply.github.com>
2022-08-10 11:16:19 +0100

    Updated Buttons in Recipe.jsx

--- src/pages/Recipe.jsx
+++ src/pages/Recipe.jsx
@@ -6,6 +6,7 @@ function Recipe() {
 
     let params = useParams();
     const [details, setDetailes] = useState({});
+    const [activeTab, setActiveTab] = useState('instractions');
 
 const fetchDetails = async () => {
     const data = await fetch(`https://api.spoonacular.com/recipes/${params.name}/information?apiKey=e86dacad0dc243289d1377a4b5dd2776`);
@@ -24,8 +25,8 @@ useEffect(() => {
         <img src={details.image} alt={details.title} />
       </div>
       <Info>
-        <Button>Instructions</Button>
-        <Button>Ingredients</Button>
+        <Button className={activeTab === 'instractions' ? 'active' : ''} onClick={() => setActiveTab('instractions')}>Instructions</Button>
+        <Button className={activeTab === 'ingredients' ? 'active' : ''} onClick={() => setActiveTab('ingredients')}>Ingredients</Button>
       </Info>
     </DetailedWrapper>
   )
