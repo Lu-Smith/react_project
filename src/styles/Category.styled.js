7dcac9a270b1b43e54845fae6b2a204d8d3dca9e
Lu-Smith <104765947+Lu-Smith@users.noreply.github.com>
2022-12-06 15:21:24 +0000

    Updated category.styled.js

--- src/styles/Category.styled.js
+++ src/styles/Category.styled.js
@@ -4,7 +4,7 @@ import {NavLink} from 'react-router-dom';
 export const List = styled.div`
 display: flex;
 justify-content:center;
-margin: 2rem 0rem;
+margin-top: 1.5rem;
 `;
 
 export const SLink = styled(NavLink)`
@@ -13,7 +13,7 @@ flex-direction: column;
 justify-content: center;
 align-items: center;
 border-radius: 50%;
-margin-right: 2rem;
+margin: 0 1rem;
 text-decoration: none;
 background: linear-gradient(35deg, #494949, #313131);
 width: 6rem;
@@ -21,28 +21,16 @@ height: 6rem;
 cursor: pointer;
 transform: scale(0.8);
 
-@media screen and (max-width: 480px) {
-    margin-right: 0.5rem;
-    width: 4rem;
-    height: 4rem;
-    padding: 1rem;
-}
-
-
 h4 {
     color: #fff;
-    font-size: 0.8rem;
-
-    @media screen and (max-width: 480px) {
-        font-size: 0.6rem;
-        margin-top: 0.3rem;
-}
+    font-size: 0.9rem;
 }
 
 svg {
     color: #fff;
-    font-size: 1.5rem;
+    font-size: 2rem;
 }
+
 &.active {
     background: linear-gradient(to right, #f27121, #e94057);
 
@@ -52,6 +40,25 @@ svg {
 
     h4 {
         color: #fff;
+    }
+}
+
+@media screen and (max-width: 820px) {
+    margin: 0 0.8rem;
+    width: 4em;
+    height: 5rem;
+    padding: 0.5rem;
+}
+
+@media screen and (max-width: 480px) {
+    margin: 0 0.5rem;
+    width: 4rem;
+    height: 4rem;
+    padding: 1rem;
+
+    h4 {
+        font-size: 0.6rem;
+        margin-top: 0.3rem;
     }
 }
 `;
