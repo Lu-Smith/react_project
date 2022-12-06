349da09fc3adad3cce6a8321ae441898962ad0a0
Lu-Smith <104765947+Lu-Smith@users.noreply.github.com>
2022-12-06 15:15:56 +0000

    Updated App.styled.js

--- src/styles/App.styled.js
+++ src/styles/App.styled.js
@@ -7,27 +7,40 @@ margin: 0 auto;
 background: #fff;
 padding: 0 30px 30px 30px;
 
-
+@media screen and (max-width: 820px) {
+  max-width: 800px;
+  padding: 0 10px 10px 10px;
+}
 `
-
 export const Logo = styled(Link)`
 text-decoration: none;
 font-size: 2rem;
 font-weight: 400;
 font-family: 'Nerko One', cursive;
 `
-
 export const Nav = styled.div`
 padding: 4rem 0;
 display: flex;
 justify-content: center;
 align-items: center;
 
-@media screen and (max-width: 480px) {
-    padding: 2rem 0;
+svg {
+    font-size: 3rem;
 }
 
-svg {
-    font-size: 2rem;
+@media screen and (max-width: 820px) {
+    padding: 3rem 0;
+
+    svg {
+       font-size: 2.5rem;
+    }
+}
+
+@media screen and (max-width: 480px) {
+    padding: 2rem 0;
+
+    svg {
+       font-size: 2rem;
+    }
 }
 `
